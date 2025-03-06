43c9d3a63a51af824c86b72cc62fc70f2a8a485e
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-06 12:45:44 +0100

    added spawning grid

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod particlePhysics;
 use core::f32;
 
 use bevy::{
-    math::{FloatPow, vec3},
+    math::{FloatPow, vec2, vec3},
     prelude::*,
 };
 use boundingBox::spawn_bounding_box;
@@ -24,18 +24,49 @@ const PARTICLE_RESOULTION: f32 = 50f32;
 const STANDARD_PARTICLE_MASS: f32 = 20f32;
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
-    spawn_particle(STANDARD_PARTICLE_MASS, PARTICLE_RAY, commands, asset_server);
+
+    for i in 0..PARTICLES_TO_SPAWN {
+        spawn_particle(
+            STANDARD_PARTICLE_MASS,
+            PARTICLE_RAY,
+            get_particle_spawn_position(i as f32),
+            &mut commands,
+            &asset_server,
+        );
+    }
 }
 
-fn spawn_particle(mass: f32, ray: f32, mut commands: Commands, asset_server: Res<AssetServer>) {
+fn spawn_particle(
+    mass: f32,
+    ray: f32,
+    pos: Vec2,
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+) {
     let sprite = Sprite::from_image(asset_server.load(CIRCLE_SPRITE_PATH));
 
     let transform = Transform {
-        translation: Vec3::new(0f32, 300f32, 0f32),
+        translation: Vec3::new(pos.x, pos.y, 0f32),
         scale: Vec3::new(ray, ray, ray),
         ..default()
     };
-    let particle = particlePhysics::Particle::new(mass, vec3(20f32, 0f32, 0f32), PARTICLE_RAY);
+    let particle = particlePhysics::Particle::new(mass, vec3(1f32, 0f32, 0f32), PARTICLE_RAY);
 
     commands.spawn((particle, transform, sprite));
+}
+//To Adjust
+const PARTICLES_TO_SPAWN: u32 = 10000;
+const PARTICLES_LAYERS: u32 = 50;
+const PARTICLES_SPACING: f32 = 20f32;
+
+const PARTICLES_SIZE_ASPECT: f32 = PARTICLES_TO_SPAWN as f32 / PARTICLES_LAYERS as f32;
+const OFFSET_VEC: Vec2 = vec2(
+    -PARTICLES_SPACING * PARTICLES_SIZE_ASPECT / 2f32,
+    -(PARTICLES_LAYERS as f32 / 2f32) * PARTICLES_SPACING,
+);
+fn get_particle_spawn_position(index: f32) -> Vec2 {
+    let y = (index / PARTICLES_SIZE_ASPECT).floor();
+    let x = index - y * PARTICLES_SIZE_ASPECT;
+
+    vec2(x, y) * PARTICLES_SPACING + OFFSET_VEC
 }
