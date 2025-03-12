9426472b5c5fdbe52147a78975a23fbbc402df29
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-12 17:50:21 +0100

    made particle grid actually have good size

--- src/particle_grid.rs
+++ src/particle_grid.rs
@@ -1,58 +1,95 @@
+use std::usize;
+
 use bevy::math::{Vec2, vec2};
 
 use crate::{
-    bounding_box::BOX_BOUNDS_SIZE_PIXELS, particles_spawning, pressure_handler::SMOOTHING_DISTANCE,
+    bounding_box::BOX_BOUNDS_SIZE_PIXELS,
+    particles_spawning::{self, PARTICLE_RAY},
+    pressure_handler::SMOOTHING_DISTANCE,
 };
-
-pub fn split_particles_into_grid(
-    particles: &[Vec2],
-) -> [Vec<usize>; particles_spawning::PARTICLES_COUNT as usize] {
+pub fn split_particles_into_grid(particles: &[Vec2]) -> [Vec<usize>; TOTAL_GRID_SIZE] {
     // maybe change to 1d vec???
-    let mut output: [Vec<usize>; particles_spawning::PARTICLES_COUNT as usize] =
-        [const { Vec::new() }; particles_spawning::PARTICLES_COUNT as usize];
+    let mut output: [Vec<usize>; TOTAL_GRID_SIZE] = [const { Vec::new() }; TOTAL_GRID_SIZE];
 
     for i in 0..particles_spawning::PARTICLES_COUNT as usize {
-        output[pos_to_grid_index(&particles[i])].push(i);
+        // println!(" pos {}", particles[i]);
+        let grid_index = pos_to_grid_index(&particles[i]);
+        if grid_index == usize::MAX {
+            continue;
+        }
+        output[grid_index].push(i);
     }
 
     output
 }
+//OK: (890 / 20 , 500 /20)  + (100 /2 ,  50 / 2) -> (95 , 50)
 pub fn pixel_pos_to_gird_pos(pixel_pos: &Vec2) -> Vec2 {
-    let raw = pixel_pos / SMOOTHING_DISTANCE;
-    vec2(raw.x.ceil(), raw.y.ceil())
+    let raw = pixel_pos / SMOOTHING_DISTANCE as f32 + vec2(GRID_SIZE_X / 2f32, GRID_SIZE_Y / 2f32);
+    vec2((raw.x as usize) as f32, (raw.y as usize) as f32)
 }
 pub fn pos_to_grid_index(pixel_pos: &Vec2) -> usize {
     grid_pos_to_index(&pixel_pos_to_gird_pos(pixel_pos))
 }
-
-const GRID_SIZE_X: f32 =
-    (BOX_BOUNDS_SIZE_PIXELS.x as u32).div_ceil(SMOOTHING_DISTANCE as u32) as f32;
-const GRID_SIZE_Y: f32 =
-    (BOX_BOUNDS_SIZE_PIXELS.y as u32).div_ceil(SMOOTHING_DISTANCE as u32) as f32;
+// 2000 / 20 -> 100
+pub const GRID_SIZE_X: f32 = (BOX_BOUNDS_SIZE_PIXELS.x as u32).div_ceil(SMOOTHING_DISTANCE) as f32;
+// 50
+pub const GRID_SIZE_Y: f32 = (BOX_BOUNDS_SIZE_PIXELS.y as u32).div_ceil(SMOOTHING_DISTANCE) as f32;
+pub const TOTAL_GRID_SIZE: usize = (GRID_SIZE_X as usize) * (GRID_SIZE_Y as usize + 1) + 1;
 
 pub fn grid_pos_to_index(grid_pos: &Vec2) -> usize {
-    ((grid_pos.y + GRID_SIZE_Y / 2f32) * GRID_SIZE_X + grid_pos.x + GRID_SIZE_X / 2f32) as usize
+    if grid_pos.x == -1f32 {
+        return usize::MAX;
+    }
+
+    ((grid_pos.y) * GRID_SIZE_X + grid_pos.x) as usize
 }
 pub fn get_connected_cells(sample_grid_pos: &Vec2) -> Vec<Vec2> {
-    vec![
-        //###
-        //#o#
-        //###
-        sample_grid_pos + vec2(-1f32, 1f32),
-        sample_grid_pos + vec2(0f32, 1f32),
-        sample_grid_pos + vec2(1f32, 1f32),
-        sample_grid_pos + vec2(-1f32, 0f32),
-        sample_grid_pos.to_owned(),
-        sample_grid_pos + vec2(1f32, 0f32),
-        sample_grid_pos + vec2(-1f32, -1f32),
-        sample_grid_pos + vec2(0f32, -1f32),
-        sample_grid_pos + vec2(1f32, -1f32),
-    ]
+    let mut output = Vec::with_capacity(9);
+    if sample_grid_pos.y < GRID_SIZE_Y {
+        // i don't have to check if x == 0 because then x == -1 so we are good
+        output.push(sample_grid_pos + vec2(-1f32, 1f32));
+
+        output.push(sample_grid_pos + vec2(0f32, 1f32));
+        if sample_grid_pos.x < GRID_SIZE_X {
+            output.push(sample_grid_pos + vec2(1f32, 1f32));
+        } else {
+            output.push(vec2(-1f32, -1f32));
+        }
+    } else {
+        output.push(vec2(-1f32, -1f32));
+        output.push(vec2(-1f32, -1f32));
+        output.push(vec2(-1f32, -1f32));
+    }
+    output.push(sample_grid_pos + vec2(-1f32, 0f32));
+    output.push(sample_grid_pos + vec2(0f32, 0f32));
+    if sample_grid_pos.x < GRID_SIZE_X {
+        output.push(sample_grid_pos + vec2(1f32, 0f32));
+    } else {
+        output.push(vec2(-1f32, -1f32));
+    }
+
+    if sample_grid_pos.y > 0f32 {
+        output.push(sample_grid_pos + vec2(-1f32, -1f32));
+        output.push(sample_grid_pos + vec2(0f32, -1f32));
+        if sample_grid_pos.x < GRID_SIZE_X {
+            output.push(sample_grid_pos + vec2(1f32, -1f32));
+        } else {
+            output.push(vec2(-1f32, -1f32));
+        }
+    } else {
+        output.push(vec2(-1f32, -1f32));
+        output.push(vec2(-1f32, -1f32));
+        output.push(vec2(-1f32, -1f32));
+    }
+
+    output
 }
 pub fn get_connected_cells_indexes(sample_grid_pos: &Vec2) -> Vec<usize> {
     let mut output: Vec<usize> = Vec::with_capacity(9);
     for pos in get_connected_cells(sample_grid_pos) {
-        output.push(grid_pos_to_index(&pos));
+        let index = grid_pos_to_index(&pos);
+
+        output.push(index);
     }
     output
 }
--- src/physics/pressure_handler.rs
+++ src/physics/pressure_handler.rs
@@ -1,5 +1,5 @@
 use crate::{
-    particle_grid::{self, pixel_pos_to_gird_pos},
+    particle_grid::TOTAL_GRID_SIZE,
     particles_spawning::{self, PARTICLES_COUNT},
 };
 use bevy::{math::Vec2, prelude::*, tasks::ParallelSlice};
@@ -7,28 +7,32 @@ use std::f32::consts::PI;
 
 // can't use SMOOTHING_DISTANCE.powi(4) so just multiply 4 times
 const SMOOTHING_KERNEL_DERIVATIVE_SCALE: f32 = 12f32
-    / (PI * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE);
+    / (PI
+        * (SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE)
+            as f32);
 
 fn smoothing_kernel_derivative(distance: f32) -> f32 {
-    if distance >= SMOOTHING_DISTANCE {
+    if distance >= SMOOTHING_DISTANCE as f32 {
         return 0f32;
     }
-    (distance - SMOOTHING_DISTANCE) * SMOOTHING_KERNEL_DERIVATIVE_SCALE
+    (distance - SMOOTHING_DISTANCE as f32) * SMOOTHING_KERNEL_DERIVATIVE_SCALE
 }
 
 // can't use SMOOTHING_DISTANCE.powi(4) so just multiply 4 times
-const SMOOTHING_KERNEL_VOLUME: f32 =
-    (PI * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE) / 6f32;
+const SMOOTHING_KERNEL_VOLUME: f32 = (PI
+    * (SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE * SMOOTHING_DISTANCE) as f32)
+    / 6f32;
 fn smoothing_kernel(distance: f32) -> f32 {
-    if distance >= SMOOTHING_DISTANCE {
+    if distance >= SMOOTHING_DISTANCE as f32 {
         return 0f32;
     }
 
-    (SMOOTHING_DISTANCE - distance) * (SMOOTHING_DISTANCE - distance) / SMOOTHING_KERNEL_VOLUME
+    (SMOOTHING_DISTANCE as f32 - distance) * (SMOOTHING_DISTANCE as f32 - distance)
+        / SMOOTHING_KERNEL_VOLUME
 }
 
 pub fn calculate_density_for_every_particle(
-    particles_gird: &[Vec<usize>; particles_spawning::PARTICLES_COUNT as usize],
+    particles_gird: &[Vec<usize>; TOTAL_GRID_SIZE],
     particles_pos: &[Vec2],
     connected_cells: &[usize],
 ) -> Vec<f32> {
@@ -62,12 +66,15 @@ pub fn calculate_pressure_force(
     sample_particel_index: usize,
     sample_connected_cells: &[usize],
     particles_pos: &[Vec2],
-    particle_grid: &[Vec<usize>; particles_spawning::PARTICLES_COUNT as usize],
+    particle_grid: &[Vec<usize>; TOTAL_GRID_SIZE],
     densities: &[f32],
 ) -> Vec2 {
     let sample_point = particles_pos[sample_particel_index];
     let mut pressure: Vec2 = Vec2::ZERO;
     for cell in sample_connected_cells {
+        if cell == &usize::MAX {
+            continue;
+        }
         for particle_index_ref in &particle_grid[cell.to_owned()] {
             let particle_index = particle_index_ref.to_owned();
             let pos = particles_pos[particle_index];
@@ -93,7 +100,7 @@ fn calculate_shared_pressure(density_a: f32, density_b: f32) -> f32 {
     let pressure_b = density_to_pressure(density_b);
     (pressure_a + pressure_b) / 2f32
 }
-pub const TARGET_DENSITY: f32 = 0.01f32;
+pub const TARGET_DENSITY: f32 = 0.08f32;
 const PRESSURE_MULTIPLIER: f32 = 1000000.0f32;
 fn density_to_pressure(density: f32) -> f32 {
     let density_error = density - TARGET_DENSITY;
@@ -104,16 +111,19 @@ fn get_influence(a: &Vec2, b: &Vec2) -> f32 {
     smoothing_kernel(a.distance(b.xy()))
 }
 
-pub const SMOOTHING_DISTANCE: f32 = 100f32;
+pub const SMOOTHING_DISTANCE: u32 = 20;
 const INFLUENCE_MODIFIER: f32 = 10f32;
 pub fn sample_density(
     sample_particle_pos: &Vec2,
     sample_connected_cells: &[usize],
-    particle_grid: &[Vec<usize>; particles_spawning::PARTICLES_COUNT as usize],
+    particle_grid: &[Vec<usize>; TOTAL_GRID_SIZE],
     particles: &[Vec2],
 ) -> f32 {
     let mut density: f32 = 0f32;
     for cell in sample_connected_cells {
+        if cell == &usize::MAX {
+            continue;
+        }
         for particle_index in &particle_grid[cell.to_owned()] {
             let influence =
                 get_influence(sample_particle_pos, &particles[particle_index.to_owned()]);
