091d81e82586e383859ac6951b835ea16e0c2c04
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-03-12 17:51:33 +0100

    disabled pressure visuals

--- src/particles_visuals.rs
+++ src/particles_visuals.rs
@@ -13,12 +13,7 @@ use bevy::{
     sprite::Sprite,
 };
 
-use crate::{
-    gradient::{ColorGradient, ColorKey},
-    particle_physics::Particle,
-    particles_spawning::PARTICLE_RAY,
-    pressure_handler,
-};
+use crate::{particle_physics::Particle, particles_spawning::PARTICLE_RAY, pressure_handler};
 const SHOW_PARTICLE_VISULAS: bool = true;
 pub fn update_particles_visuals(
     mut particles: Query<(&mut Transform, &Particle, &mut Sprite)>,
@@ -35,7 +30,7 @@ pub fn update_particles_visuals(
             sprite.color = Color::Srgba(Srgba::lerp(DARK_BLUE, LIGHT_GREEN, t));
 
             let scale = PARTICLE_RAY
-                * (pressure_handler::TARGET_DENSITY / particle.density).clamp(0.1f32, 3f32);
+                /* * (pressure_handler::TARGET_DENSITY / particle.density).clamp(0.1f32, 3f32) */;
             transform.scale = vec3(scale, scale, 0f32);
         });
 }
